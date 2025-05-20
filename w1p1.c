6458f6802218ddfcfaaaf6328d4251053efca3ed
Dil1797 <147470344+Dil1797@users.noreply.github.com>
2025-05-19 22:55:28 -0400

    Add files via upload

---
+++ w1p1.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+                       Workshop - #1 (Part - 1)
+Full Name   :Dil Humyra Sultana Borna
+Student ID# :139166227
+Email       :dhsborna@myseneca.ca
+Section     :NEE
+
+Authenticity Declaration :
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider.This submitted
+piece of work is entirely of my own creation.
+******************************************************************************/
+
+#include <stdio.h>
+
+int main (Void)
+{
+    printf("Workshop 1 Part-1\n");
+    printf("=================\n\n");
+    printf("I'm displaying this using the 'printf' stdio\n");
+    printf("(standard input output) library function!\n\n");
+    printf("Dear teacher,\n\n");
+    printf("  I promise I will work hard from this day onward.\n");
+    printf("  I acknowledge that practice is extremely important,\n");
+    printf("  so I will do all workshops, quizzes, and assignments.\n\n");
+    printf("Sincerely,\n\n");
+    printf("Dil Humyra Sultana Borna\n");
+    printf("Student ID# 139166227\n");
+    return 0;
+}
+
+
